14421696b427b24268aa67c95287a7c4badbaead
GustavoCKruger <gustavo.kruger1601@gmail.com>
2025-09-02 14:27:09 -0300

    feat: Gravação do preço do livro no item da compra

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,7 +1,7 @@
 from rest_framework.serializers import (
     CharField,
-    CurrentUserDefault, 
-    HiddenField,       
+    CurrentUserDefault,
+    HiddenField,
     ModelSerializer,
     SerializerMethodField,
     ValidationError,
@@ -13,13 +13,13 @@ from core.models import Compra, ItensCompra
 class ItensCompraCreateUpdateSerializer(ModelSerializer):
     class Meta:
         model = ItensCompra
-        fields = ('livro', 'quantidade')
-        
+        fields = ('livro', 'quantidade', 'preco')
+
     def validate_quantidade(self, quantidade):
         if quantidade <= 0:
             raise ValidationError('A quantidade deve ser maior do que zero.')
         return quantidade
-    
+
     def validate(self, item):
         if item['quantidade'] > item['livro'].quantidade:
             raise ValidationError('Quantidade de itens maior do que a quantidade em estoque.')
@@ -32,31 +32,35 @@ class CompraCreateUpdateSerializer(ModelSerializer):
 
     class Meta:
         model = Compra
-        fields = ('id', 'usuario', 'itens')
+        fields = ('usuario', 'itens')
 
     def create(self, validated_data):
         itens_data = validated_data.pop('itens')
         compra = Compra.objects.create(**validated_data)
         for item_data in itens_data:
+            item_data['preco'] = item_data['livro'].preco  # preço do livro no momento da compra
             ItensCompra.objects.create(compra=compra, **item_data)
         compra.save()
         return compra
-    
+
     def update(self, compra, validated_data):
         itens_data = validated_data.pop('itens', None)
         if itens_data is not None:
             compra.itens.all().delete()
             for item_data in itens_data:
+                item_data['preco'] = item_data['livro'].preco  # preço do livro no momento da compra
                 ItensCompra.objects.create(compra=compra, **item_data)
         return super().update(compra, validated_data)
-    
+
+
 class ItensCompraListSerializer(ModelSerializer):
     livro = CharField(source='livro.titulo', read_only=True)
 
     class Meta:
         model = ItensCompra
-        fields = ('quantidade', 'livro')
+        fields = ('quantidade', 'preco', 'livro')
         depth = 1
+
 
 class CompraListSerializer(ModelSerializer):
     usuario = CharField(source='usuario.email', read_only=True)
@@ -66,16 +70,18 @@ class CompraListSerializer(ModelSerializer):
         model = Compra
         fields = ('id', 'usuario', 'itens')
 
+
 class ItensCompraSerializer(ModelSerializer):
     total = SerializerMethodField()
 
     def get_total(self, instance):
-        return instance.livro.preco * instance.quantidade
+        return instance.quantidade * instance.preco
 
     class Meta:
         model = ItensCompra
-        fields = ('id', 'livro', 'quantidade', 'total')
-        depth = 1
+        fields = ('id', 'quantidade', 'preco', 'total', 'livro')
+        depth = 2
+
 
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source='usuario.email', read_only=True)
