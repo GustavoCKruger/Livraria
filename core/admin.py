64f5e84562a0b81bcad928bc83eb428dbbea8a65
GustavoCKruger <gustavo.kruger1601@gmail.com>
2025-09-09 14:19:33 -0300

    feat: registrando a data da compra

--- core/admin.py
+++ core/admin.py
@@ -89,12 +89,14 @@ class ItensCompraInline(admin.TabularInline):
 
 @admin.register(Compra)
 class CompraAdmin(admin.ModelAdmin):
-    list_display = ('usuario', 'status', 'total_formatado')  # mostra na listagem
-    ordering = ('usuario', 'status')
+    list_display = ('usuario', 'status', 'total_formatado', 'data')  # mostra na listagem
+    ordering = ('usuario', 'status', 'data')  # ordena por esses campos
+    search_fields = ('usuario__email', 'status')  # campos pesquisáveis
+    list_filter = ('status', 'data')  # filtros laterais
     list_per_page = 10
     inlines = [ItensCompraInline]
-    readonly_fields = ("total_formatado",)  # mostra dentro do formulário
-
+    readonly_fields = ('data', 'total_formatado',)  # campos somente leitura
+    
     @admin.display(description="Total")
     def total_formatado(self, obj):
         """Exibe R$ 123,45 em vez de 123.45."""
